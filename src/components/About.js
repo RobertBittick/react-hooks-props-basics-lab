ea799a6957d24113e7d71b767c28afbbea14b438
RobertBittick <bobbybittick@Robert-Laptop.localdomain>
2022-11-24 01:19:24 -0800

    commit tree 06f8ad5f7d453e03fbd839924b31471742b323d5

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import user from "../data/user";
+
+function Links(){
+  return(
+    <div id ="links">
+      <h3>Links</h3>
+      <a href={user.links.github}>https://github.com/liza</a>
+      <a href={user.links.linkedin}>https://www.linkedin.com/in/liza/</a>
+    </div>
+  )
+        
+}
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio ? <p>{user.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
